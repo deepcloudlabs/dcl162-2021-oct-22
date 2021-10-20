4c71872109a731be38515c90af6f28a8da291af1
deepcloudlabs <deepcloudlabs@gmail.com>
2021-10-20 16:13:04 +0300

    initial commit

---
+++ module02-functional.programming.in.python/banking/domain.py
@@ -0,0 +1,64 @@
+class InsufficientBalanceError(Exception):  # inheritance
+    def __init__(self, message, deficit):
+        self.message = message
+        self.deficit = deficit
+
+
+class Account:
+    def __init__(self, iban, balance):
+        self._iban = iban
+        self._balance = balance
+
+    @property
+    def iban(self):
+        return self._iban
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def withdraw(self, amount):
+        print("Account::withdraw")
+        if amount <= 0:  # validation
+            raise ValueError("amount must be positive")
+        if amount > self._balance:  # business rule
+            deficit = amount - self._balance
+            raise InsufficientBalanceError("your balance does cover your expenses", deficit)
+        self._balance = self._balance - amount
+
+    def deposit(self, amount):
+        if amount <= 0:  # validation
+            raise ValueError("amount must be positive")
+        self._balance = self._balance + amount
+
+    def __str__(self):
+        return f"Account [iban: {self._iban}, balance: {self._balance}]"
+
+
+class CheckingAccount(Account):
+    """
+    CheckingAccount -> Derived Class, Sub-class
+    Account         -> Base Class   , Super-class
+    """
+
+    def __init__(self, iban, balance, overdraft_amount=500):
+        super().__init__(iban, balance)
+        self._overdraft_amount = overdraft_amount
+
+    @property
+    def overdraft_amount(self):
+        return self._overdraft_amount
+
+    # overriding
+    def withdraw(self, amount):
+        print("CheckingAccount::withdraw")
+        if amount <= 0:  # validation
+            raise ValueError("amount must be positive")
+        if amount > (self._balance + self._overdraft_amount):  # business rule
+            deficit = amount - self._balance - self._overdraft_amount
+            raise InsufficientBalanceError("your balance does cover your expenses", deficit)
+        self._balance = self._balance - amount
+
+    # overriding
+    def __str__(self):
+        return f"CheckingAccount [iban: {self._iban}, balance: {self._balance}, overdraft_amount: {self._overdraft_amount}]"
---
+++ module02-functional.programming.in.python/exercise5.py
@@ -0,0 +1,12 @@
+from banking.domain import Account, CheckingAccount
+
+accounts = [
+    Account("tr1", 5000),
+    CheckingAccount("tr2", 5000, 1000),
+    Account("tr3", 3000),
+    CheckingAccount("tr4", 4000, 500)
+]
+
+# total balance
+
+# number of Account objects
