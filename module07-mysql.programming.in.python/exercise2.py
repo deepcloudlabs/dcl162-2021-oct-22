aa1a23ec66aff3f5de1183e9df3ede7b5c3ec2c6
deepcloudlabs <deepcloudlabs@gmail.com>
2021-10-22 12:09:23 +0300

    Update exercise2.py

--- module07-mysql.programming.in.python/exercise2.py
+++ module07-mysql.programming.in.python/exercise2.py
@@ -1,4 +1,17 @@
 import mysql.connector
 
 conn = mysql.connector.connect(host="localhost", user="root",
-                               password="Secret_123", database="world")
+                               password="Secret_123", database="hrdb")
+
+cur = conn.cursor()
+
+# cur.execute("create database hrdb")
+
+cur.execute("""
+    create table employees(
+        id int(11) auto_increment primary key,
+        full_name varchar(128) not null,
+        iban varchar(128) not null,
+        salary int(10)
+    ) engine=innodb    
+""")
